e90b016aaf04fde8738069aba10a15e6cb8a64db
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-12 20:55:41 -0300

    consumindo msg da api boleto

--- mensageria/src/main/java/com/example/consumer/mensageria/MensageriaApplication.java
+++ mensageria/src/main/java/com/example/consumer/mensageria/MensageriaApplication.java
@@ -2,8 +2,10 @@ package com.example.consumer.mensageria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class MensageriaApplication {
 
 	public static void main(String[] args) {
--- mensageria/src/main/java/com/example/consumer/mensageria/service/kafka/BoletoConsumer.java
+++ mensageria/src/main/java/com/example/consumer/mensageria/service/kafka/BoletoConsumer.java
@@ -1,5 +1,19 @@
 package com.example.consumer.mensageria.service.kafka;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.example.messageria.avro.Boleto;
+
+@Service
 public class BoletoConsumer {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
+
+  @KafkaListener(topics = "${spring.kafka.topico-boleto}", groupId = "${spring.kafka.consumer.group-id}")
+  public void consomeBoleto(Boleto boleto) {
+    LOGGER.info(String.format("Consumindo mensagem -> %s", boleto));
+  }
 }
