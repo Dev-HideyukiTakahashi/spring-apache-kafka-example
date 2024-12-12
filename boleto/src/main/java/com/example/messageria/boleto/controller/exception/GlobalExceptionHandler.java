bf5941d473e5170b4e1cbd45fd35f0354cfe9b34
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-12 19:09:50 -0300

    config kafka

--- boleto/src/main/java/com/example/messageria/boleto/controller/exception/GlobalExceptionHandler.java
+++ boleto/src/main/java/com/example/messageria/boleto/controller/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.example.messageria.boleto.controller.exception;
 
 import java.util.Date;
-import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
