5d0c1210db7ac44f756271f1a7dd87d24b794ad2
Hideyuki Takahashi <dev.hideyukitakahashi@gmail.com>
2024-12-13 01:03:17 -0300

    config para rastrear apenas new msg

--- mensageria/src/main/java/com/example/consumer/mensageria/service/kafka/BoletoConsumer.java
+++ mensageria/src/main/java/com/example/consumer/mensageria/service/kafka/BoletoConsumer.java
@@ -1,14 +1,18 @@
 package com.example.consumer.mensageria.service.kafka;
 
+import java.util.Map;
+
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 
 import com.example.messageria.avro.Boleto;
 
 @Service
-public class BoletoConsumer {
+public class BoletoConsumer implements ConsumerSeekAware {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
 
@@ -16,4 +20,11 @@ public class BoletoConsumer {
   public void consomeBoleto(Boleto boleto) {
     LOGGER.info(String.format("Consumindo mensagem -> %s", boleto));
   }
+
+  // config para rastrear apenas mensagens novas
+  @Override
+  public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+    assignments.forEach((t, o) -> callback.seekToEnd(t.topic(), t.partition()));
+  }
+
 }
